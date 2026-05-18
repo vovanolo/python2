b7d4a31cc7edd8ddd4ef75c8066b00c1b2611c57
Volodymyr <volodymyr@Mac.lan>
2026-05-18 19:15:41 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,64 @@
+# print("hello world")
+
+# start excercise 1
+
+# a = 10
+# b = 20
+# print(a + b)
+
+# end excercise 1
+
+
+# start excercise 2
+# a = 2
+# b = 2
+# print(a * b)
+# end excercise 2
+
+# умови в пайтоні
+
+# age = 10
+
+# if умова є true:
+#   виконай код який знаходиться в тілі if
+# if age >= 18:
+#     print("age is greater than 18")
+
+
+
+
+# if age >= 18:
+#     print("age is greater than 18")
+# else:
+#     print("age is less than 18")
+
+
+# age = int(input("enter your age: "))
+
+# print(type(age))
+
+# if age >= 18:
+#     print("age is greater than 18")
+# else:
+#     print("age is less than 18")
+
+# if age > 18:
+#     print("age is greater than 18")
+# elif age < 18:
+#     print("age is less than 18")
+# else:
+#     print("age is equal to 18")
+
+
+# age > 20 and age < 30
+
+age = int(input("enter your age: "))
+
+if age > 20 and age < 30:
+    print("age is between 20 and 30")
+elif age > 30 and age < 40:
+    print("age is between 30 and 40")
+elif age > 40 and age < 50:
+    print("age is between 40 and 50")
+else:
+    print("age is less than 20 or greater than 50")
